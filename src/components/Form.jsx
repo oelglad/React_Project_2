16770583ea6fd1314942644dd478d75fcd8da363
oelglad <omnia_elglad@aol.com>
2019-12-19 22:07:26 -0500

    Updating [Form][App.Css]

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 import ComponentChart from './ComponentChart';
 
 function Form(props) {
-  return(
-      <div>
+  return (
+    <div>
       <form className="search" onSubmit={props.onClick}>
-      {/* <input type="text" name="0" onChange={props.onChange} placeholder="search date" name="date"/> */}
-      <input type="text" name="0" onChange={props.onChange} placeholder="search company" name="rightnow"/>
-      <input type="submit" value="submit!"></input>
-      {/* <button onClick={props.onClick}>Breeds list</button> */}
+        {/* <input type="text" name="0" onChange={props.onChange} placeholder="search date" name="date"/> */}
+        {/* <input type="text" name="0" onChange={props.onChange} placeholder="search company" name="rightnow" /> */}
+        <select id="country" onChange={props.onChange} name="rightnow" >
+          <option value={props.rightnow}>MSFT</option>
+          <option value={props.rightnow}>NDAQ</option>
+          <option value={props.rightnow}>goog</option>
+        </select>
+       
+        <input type="submit" value="submit!"></input>
+        {/* <button onClick={props.onClick}>Breeds list</button> */}
       </form>
-      </div>
+    </div>
   )
 }
 
 export default Form;
-{/* <button onClick={props.onClick}>Breeds list</button> */}
+{/* <button onClick={props.onClick}>Breeds list</button> */ }
